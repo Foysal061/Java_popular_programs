91280a666e0e692f599ad1112f780418bf43a6a6
Foysal061 <ariful.foysal@gmail.com>
2022-10-21 03:19:42 +0200

    Find third highest salary done and notes count modified

--- src/java_array_function/NotesCountFromAmount.java
+++ src/java_array_function/NotesCountFromAmount.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 
 public class NotesCountFromAmount {
     public static void main(String[] args) {
-        try{
-            int notes[] = {1000,500,200,100,50,20,10,5,2,1};
+        try {
+            int[] notes = {1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
             System.out.print("Enter amount: ");
             Scanner sc = new Scanner(System.in);
             int amount = sc.nextInt();
-            for(int note:notes){
+            for (int note : notes) {
                 int noOfNotes = amount / note;
-                if(noOfNotes > 0){
-                    System.out.println(""+note+" "+noOfNotes);
+                if (noOfNotes > 0) {
+                    System.out.println("" + note + " " + noOfNotes);
                 }
                 amount = amount % note;
             }
-        } catch (Exception error){
-            System.out.println("Invalid input, please enter integer number.\n"+error);
+        } catch (Exception error) {
+            System.out.println("Invalid input, please enter integer number.\n" + error);
         }
 
     }
